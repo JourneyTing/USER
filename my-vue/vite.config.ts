eaf74bc41e903433ca4d49b9aeea65ce78c08d37
🫠🫠🫠 <123@123.12>
2025-01-17 16:49:27 +0900

    setting

--- my-vue/vite.config.ts
+++ my-vue/vite.config.ts
@@ -4,14 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-vue',
-  plugins: [vue(), vueJsx(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/my-vue',
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss]
+        }
     }
-  }
 })
